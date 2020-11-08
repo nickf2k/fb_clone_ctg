a63038816e6ab5e45035f20ca66bfa09edb214bd
vuong.nguyen <vuong.nguyen@sea-solutions.com>
2020-11-08 16:45:36 +0700

    add file

---
+++ android/app/src/main/kotlin/com/ctg/fb_clone_ctg/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ctg.fb_clone_ctg
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
