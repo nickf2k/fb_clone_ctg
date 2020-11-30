03d3bc9de5bb056ea6074e73f9528e777148a409
vuong.nguyen <vuong.nguyen@sea-solutions.com>
2020-11-30 16:53:51 +0700

    add pagecontainer

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData themeConfig() {
   return ThemeData(
       primarySwatch: Colors.cyan,
-      fontFamily: 'icomoon',
+      fontFamily: 'icielpanton',
       textTheme: TextTheme(
         body1: TextStyle(
           fontWeight: FontWeight.w600,
