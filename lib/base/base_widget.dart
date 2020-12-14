9a66194da78503b43f6d4ed8facc2beee176632d
toanyb <toannguyen100998@gmail.com>
2020-12-14 18:45:27 +0700

    page container with animation

--- lib/base/base_widget.dart
+++ lib/base/base_widget.dart
@@ -1,38 +1,32 @@
 import 'package:fb_clone_ctg/config/app_color.dart';
 import 'package:fb_clone_ctg/constant/route_constant.dart';
 import 'package:fb_clone_ctg/constant/spref_constant.dart';
+import 'package:fb_clone_ctg/shared/widgets/custom_app_bar.dart';
+import 'package:fb_clone_ctg/shared/widgets/search_bar.dart';
 import 'package:fb_clone_ctg/untils/spref_util.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../module/setting/settingg.dart';
+import '../shared/widgets/top_nav_bar.dart';
+
 class PageContainer extends StatelessWidget {
-  final String title;
   final Widget child;
-  final List<Widget> actions;
-  final List<SingleChildCloneableWidget> bloc;
-  final List<SingleChildCloneableWidget> di;
-  final Widget sidebar;
+
   final Widget topNavBar;
-  final Widget specialTitle;
   final bool canSearch;
 
   PageContainer({
-    this.title,
-    this.actions,
     this.child,
-    this.bloc,
-    this.di,
-    this.sidebar,
     this.topNavBar,
-    this.specialTitle,
     this.canSearch = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (SpUtil.getString(SPrefCacheConstant.KEY_TOKEN).isEmpty)
-      Navigator.pushReplacementNamed(context, RouteConstant.SIGN_IN);
+    // if (SpUtil.getString(SPrefCacheConstant.KEY_TOKEN).isEmpty)
+    //   Navigator.pushReplacementNamed(context, RouteConstant.SIGN_IN);
     final _scaffoldKey = GlobalKey<ScaffoldState>();
 //    if (actions == null) actions = List<Widget>();
 //    actions.add(IconButton(
@@ -41,42 +35,44 @@ class PageContainer extends StatelessWidget {
 ////        showSearch(context: context, delegate: SearchBar());
 //      },
 //    ));
+
     return Scaffold(
-      body: MultiProvider(
-        providers: [...di, ...bloc],
-        child: SafeArea(
+      body: SafeArea(
           bottom: false,
           child: Scaffold(
             key: _scaffoldKey,
-            drawer: sidebar,
             body: child,
-            appBar: PreferredSize(
-              
+            appBar: CustomAppBar(
+              height: 100,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[SearchBar(), TopNavBar()],
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
+          )),
     );
   }
 
-  Widget _getAppbarTitle(context) {
-    if (this.specialTitle != null) {
-      return this.specialTitle;
-    }
-    return this.title != null ? _buildTitle() : _buildSearchBox(context);
-  }
-
-  Text _buildTitle() {
-    return Text(
-      this.title,
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        color: AppColor.blueMain,
-        fontWeight: FontWeight.w600,
-        fontSize: 17,
-      ),
-    );
-  }
+  // Widget _getAppbarTitle(context) {
+  //   if (this.specialTitle != null) {
+  //     return this.specialTitle;
+  //   }
+  //   return this.title != null ? _buildTitle() : _buildSearchBox(context);
+  // }
+  //
+  // Text _buildTitle() {
+  //   return Text(
+  //     this.title,
+  //     textAlign: TextAlign.center,
+  //     style: TextStyle(
+  //       color: AppColor.blueMain,
+  //       fontWeight: FontWeight.w600,
+  //       fontSize: 17,
+  //     ),
+  //   );
+  // }
 
   Widget _buildSearchBox(BuildContext context) {
     // return SearchBox(canSearch);
---
+++ lib/module/home_page/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../base/base_widget.dart';
+import '../../base/base_widget.dart';
+import '../../shared/widgets/top_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return PageContainer(
+      child: Container(),
+    );
+    // return SafeArea(child: Scaffold(body: TopNavBar()));
+  }
+}
