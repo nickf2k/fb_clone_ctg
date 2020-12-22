fff38ad63f8d87aea82a16dfab1b7eaa7a5b1934
vuong.nguyen <vuong.nguyen@sea-solutions.com>
2020-12-22 15:49:57 +0700

    fig setting/settingg.dart, add asserts, icon

--- lib/config/route.dart
+++ lib/config/route.dart
@@ -1,6 +1,7 @@
 import 'package:fb_clone_ctg/constant/route_constant.dart';
 import 'package:fb_clone_ctg/module/add_friend/add_fr_view.dart';
 import 'package:fb_clone_ctg/module/home_page/home_view.dart';
+import 'package:fb_clone_ctg/module/setting/settingg.dart';
 import 'package:fb_clone_ctg/module/sign_in/sign_in_view.dart';
 import 'package:flutter/material.dart';
 
@@ -9,13 +10,11 @@ import '../module/notification/notification_view.dart';
 
 Map<String, WidgetBuilder> routeConfig() {
   return <String, WidgetBuilder>{
-
     RouteConstant.SIGN_IN: (context) => SignInPage(),
     RouteConstant.NOTIFICATIONS: (context) => NotificationPage(),
     RouteConstant.HOME: (context) => HomePage(),
-
+    RouteConstant.SETTING: (context) => SettingPage(),
     RouteConstant.VIDEO: (context) => HomePage(),
     RouteConstant.ADD_FRIEND: (context) => AddFriendPage(),
-
   };
 }
