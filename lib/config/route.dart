13afa81ac4b3ae37981f5ceb50fb067b697d3224
Phạm Tuấn Nghĩa <52267082+phamtuannghia22@users.noreply.github.com>
2021-01-03 16:57:50 +0700

    finished view 5 ,9 in setting

--- lib/config/route.dart
+++ lib/config/route.dart
@@ -4,6 +4,8 @@ import 'package:fb_clone_ctg/module/home_page/home_view.dart';
 import 'package:fb_clone_ctg/module/post/add_post/add_post_view.dart';
 import 'package:fb_clone_ctg/module/post/post_view.dart';
 import 'package:fb_clone_ctg/module/profile/profile_view.dart';
+import 'package:fb_clone_ctg/module/setting/setting_information/changing_password_view.dart';
+import 'package:fb_clone_ctg/module/setting/setting_information/setting_information_view.dart';
 import 'package:fb_clone_ctg/module/setting/setting_view.dart';
 import 'package:fb_clone_ctg/module/sign_up/change_info/change_info_view.dart';
 import 'package:fb_clone_ctg/module/videos/videos_view.dart';
@@ -25,7 +27,8 @@ Map<String, WidgetBuilder> routeConfig() {
     RouteConstant.ADD_POST: (context) => AddPostPage(),
     RouteConstant.SETTING: (context) => SettingPage(),
     RouteConstant.VIDEO: (context) => VideosPage(),
-    // RouteConstant.SETTING_INFO: (context) => SettingInfoPage(),
+    RouteConstant.SETTING_INFO: (context) =>  SettingInformationPage(),
+    RouteConstant.CHANGE_PASSWORD: (context) => ChangingPasswordPage(),
     RouteConstant.PROFILE: (context) => ProfilePage(),
     RouteConstant.SIGN_UP: (context) => SignUpPage(),
     RouteConstant.CHANGE_INFO: (context) => ChangeInfoPage(),
